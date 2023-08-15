8e23073a5f52450f0865360a3f362be0d9918477
TemaGolovin <golart1996@mail.ru>
2023-08-15 13:13:55 +0700

    update

--- src/helpers/setAdaptive.js
+++ src/helpers/setAdaptive.js
@@ -4,6 +4,7 @@ export default (columns) => {
   document.body.style.setProperty('--width-cell', '32px');
   document.body.style.setProperty('--border-cell', '3px solid rgb(36, 66, 56)');
   document.body.style.setProperty('--outline-open', '1px solid rgb(107, 214, 179)');
+  document.body.style.setProperty('--font-size', '16px');
   if (widthView < 1140) {
     if (columns > 25) {
       document.body.style.setProperty('--width-cell', '28px');
@@ -11,7 +12,7 @@ export default (columns) => {
     }
   }
   if (widthView < 870) {
-    if (columns >= 25) {
+    if (columns >= 19) {
       document.body.style.setProperty('--width-cell', '25px');
       document.body.style.setProperty('--border-collapse', 'collapse');
       document.body.style.setProperty('--border-cell', '1px solid rgb(107, 214, 179)');
@@ -21,13 +22,17 @@ export default (columns) => {
     }
   }
   if (widthView < 480) {
-    if (columns >= 25) {
-      document.body.style.setProperty('--width-cell', '17px');
+    if (columns >= 19) {
+      document.body.style.setProperty('--font-size', '8px');
+      document.body.style.setProperty('--width-cell', '16px');
       document.body.style.setProperty('--border-collapse', 'collapse');
-      document.body.style.setProperty('--border-cell', '1px solid rgb(107, 214, 179)');
-      document.body.style.setProperty('--outline-open', '1px solid rgb(107, 214, 179)');
-    } else if (columns >= 16 && columns < 25) {
-      document.body.style.setProperty('--width-cell', '24px');
+      document.body.style.setProperty('--border-cell', '0.2px solid rgb(107, 214, 179)');
+      document.body.style.setProperty('--border-cell-top', '0.2px solid rgb(107, 214, 179)');
+      document.body.style.setProperty('--border-cell-left', '0.2px solid rgb(107, 214, 179)');
+      document.body.style.setProperty('--outline-open', '0.2px solid rgb(107, 214, 179)');
+    } else if (columns >= 12 && columns < 19) {
+      document.body.style.setProperty('--font-size', '10px');
+      document.body.style.setProperty('--width-cell', '20px');
       document.body.style.setProperty('--border-collapse', 'collapse');
       document.body.style.setProperty('--border-cell', '1px solid rgb(107, 214, 179)');
       document.body.style.setProperty('--outline-open', '1px solid rgb(107, 214, 179)');
